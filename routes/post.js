6bcf771d0b529d2235ab5df16c98df02aca5329e
VinaySehwag14 <vinaysehwag14@gmail.com>
2021-11-27 01:47:44 +0530

    CATEGORY  QUERY SOLVED

--- routes/post.js
+++ routes/post.js
@@ -81,11 +81,18 @@ router.get("/:id", async (req, res) => {
 
 //*Get all post
 router.get("/", async (req, res) => {
+  const username = req.query.user;
+  const catName = req.query.cat;
   try {
-    const username = req.query.user;
     let posts;
     if (username) {
       posts = await Post.find({ username: username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          $in: [catName],
+        },
+      });
     } else {
       posts = await Post.find();
     }
